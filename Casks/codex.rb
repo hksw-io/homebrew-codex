135613f608069c4e3399a0f51581f28903c01b69
Robin Hallabro-Kokko <robin@hallabro-kokko.se>
2026-07-08 01:35:18 +0000

    chore(cask): update codex to 0.143.0

--- Casks/codex.rb
+++ Casks/codex.rb
@@ -1,5 +1,5 @@
 cask "codex" do
-  version "0.143.0-alpha.39"
+  version "0.143.0"
   name "Codex"
   desc "OpenAI's coding agent that runs in your terminal"
   homepage "https://github.com/openai/codex"
@@ -14,23 +14,23 @@ cask "codex" do
 
   if OS.mac?
     if Hardware::CPU.arm?
-      sha256 "37052b7239e88cdabe5eb224872f8693914b6933ada29655e19f5e4c68ce26ad"
+      sha256 "7df2384f037519dff7dbf4252e60913a5c1c7fdb66c1467c9125b2b2d3594a86"
       url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-aarch64-apple-darwin.tar.gz"
       binary "codex-aarch64-apple-darwin", target: "codex"
       generate_completions_from_executable "codex-aarch64-apple-darwin", "completion", base_name: "codex"
     else
-      sha256 "c6583807d74d02189811f809e758730e724eca8cd1922d103650ffe117d617af"
+      sha256 "f776ef1d5e5f03151dd6e6af94cc7b13e9b08c3c52bd60327692009b01dbfb4a"
       url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-x86_64-apple-darwin.tar.gz"
       binary "codex-x86_64-apple-darwin", target: "codex"
       generate_completions_from_executable "codex-x86_64-apple-darwin", "completion", base_name: "codex"
     end
   elsif Hardware::CPU.arm?
-    sha256 "eb37ab966d059b93f20d3db5cbbdccd792a80a49f19c52d501931626ee5c2498"
+    sha256 "daec4cb18f3ad3e541e8986763e73019b3ddb726d1f8692e50ea5fa988c09d78"
     url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-aarch64-unknown-linux-musl.tar.gz"
     binary "codex-aarch64-unknown-linux-musl", target: "codex"
     generate_completions_from_executable "codex-aarch64-unknown-linux-musl", "completion", base_name: "codex"
   else
-    sha256 "1161b7fc24684983f8b6fa83a53dcbc8c155b8494097375182bb17e4928305d8"
+    sha256 "d9dc731dc66e227b175b13c071be9ea126cc7672fcac8a87823da50b0d2b2ffe"
     url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-x86_64-unknown-linux-musl.tar.gz"
     binary "codex-x86_64-unknown-linux-musl", target: "codex"
     generate_completions_from_executable "codex-x86_64-unknown-linux-musl", "completion", base_name: "codex"
