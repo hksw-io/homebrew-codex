97a0725b7580ef68da4c4fad54b850a04ecf67a7
Robin Hallabro-Kokko <robin@hallabro-kokko.se>
2026-07-23 00:50:53 +0000

    chore(cask): update codex to 0.146.0-alpha.4

--- Casks/codex.rb
+++ Casks/codex.rb
@@ -1,5 +1,5 @@
 cask "codex" do
-  version "0.146.0-alpha.3"
+  version "0.146.0-alpha.4"
   name "Codex"
   desc "OpenAI's coding agent that runs in your terminal"
   homepage "https://github.com/openai/codex"
@@ -14,17 +14,17 @@ cask "codex" do
 
   if OS.mac?
     if Hardware::CPU.arm?
-      sha256 "e989288e0c00a9277ed155b691756e99791d6739a2b7832a5dac4a86c3c0ede0"
+      sha256 "3c854b3b8ac4fe8a604a0d6546f5c02aa09466246df5686863b02b1e88f450ac"
       url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-aarch64-apple-darwin.tar.gz"
     else
-      sha256 "98d5ca6efebecdd4fd5dd6a84f95facea9f93d254b17959cdb5ba9945ba04549"
+      sha256 "9e9cf187939494d01abdd12749c764c1864e674f2f4241139596d2c5f5372cb9"
       url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-x86_64-apple-darwin.tar.gz"
     end
   elsif Hardware::CPU.arm?
-    sha256 "f436d723c905bc176f0a042fcf8f63d7d5deb8b3480c78da5c4722898f7fcde2"
+    sha256 "4644b160cf61037419923fb1a1298fb0d53ec2794893e95fc7bc89c3052ae5a8"
     url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-aarch64-unknown-linux-musl.tar.gz"
   else
-    sha256 "4f695e6245232ef4483444afe51fcadfad67cf0c511de7f961dba990a0ef557d"
+    sha256 "054f3bd5f0a99c430844f0b8854bed91dfd8e35dee55c5b48e3c4e81a62c9c51"
     url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-x86_64-unknown-linux-musl.tar.gz"
   end
 
