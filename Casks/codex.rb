82ff8fc6acddc86e59f1fa45312a0e4acf4c8568
Robin Hallabro-Kokko <robin@hallabro-kokko.se>
2026-07-30 09:29:11 +0000

    chore(cask): update codex to 0.146.0

--- Casks/codex.rb
+++ Casks/codex.rb
@@ -2,11 +2,11 @@ cask "codex" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "apple-darwin", linux: "unknown-linux-musl"
 
-  version "0.146.0-alpha.14"
-  sha256 arm:          "6c6cc6fb3ef1ebb8cbc76fd8bbfc7489f04ec421fff5daa855dd4844455d8546",
-         intel:        "d4130d0439bac40932d363ea247b86bdc2759c92e419ae496c1e04c5ddc14805",
-         arm64_linux:  "28f2b3a0472ea56bfd3f6317e3de65b68636928bb02c6cf53a05dbe5ed975763",
-         x86_64_linux: "32d012b55306448c61d0de3db7db3a10eef6a9257b184dafad02a5e63530c6e3"
+  version "0.146.0"
+  sha256 arm:          "cd961b480f6dfc4703bd244601f1927231fa31a587cb9046ccdffa6c4c29e7d5",
+         intel:        "f72f5ab71729e90b8e86343e9199c0f7a7eebbca5d6b1fc4cfcdaf35a3e5b641",
+         arm64_linux:  "c6eb28ec19bb5615b60e6787165ef28482481c2ce2617da565b83e591bc44c13",
+         x86_64_linux: "3c89125af1d7c98abec8beb551292ef99daca52e204e5852a9139feae2c467e5"
 
   url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-package-#{arch}-#{os}.tar.gz"
   name "Codex"
